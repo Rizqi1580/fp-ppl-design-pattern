2ddae7b2d617fc3059fb2f60a9e9258163b87404
Rizqi1580 <126391828+Rizqi1580@users.noreply.github.com>
2025-06-12 01:18:57 +0700

    Update strategy.ts

--- strategy.ts
+++ strategy.ts
@@ -1,32 +1,32 @@
-interface StatStrategy {
+interface StatStrategy{
   calculate(requests: Request[]): Statistics;
 }
 
-type Request = {
+type Request ={
   time_request: Date;
   amount: number;
 };
 
-type Statistics = {
+type Statistics ={
   period: string;
   totalAmount: number;
   count: number;
 };
 
-class YearlyStatStrategy implements StatStrategy {
-  calculate(requests: Request[]): Statistics {
+class YearlyStatStrategy implements StatStrategy{
+  calculate(requests: Request[]): Statistics{
     // Logic hitung statistik per tahun
     };
   }
 }
-class MonthlyStatStrategy implements StatStrategy {
-  calculate(requests: Request[]): Statistics {
+class MonthlyStatStrategy implements StatStrategy{
+  calculate(requests: Request[]): Statistics{
     // Logic hitung statistik per bulan
     };
   }
 }
-class WeeklyStatStrategy implements StatStrategy {
-  calculate(requests: Request[]): Statistics {
+class WeeklyStatStrategy implements StatStrategy{
+  calculate(requests: Request[]): Statistics{
     // Logic hitung statistik per minggu
     };
   }
