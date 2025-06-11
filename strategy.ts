dc3a822212cced6f50455c6aef67be4c55711cc8
Rizqi1580 <126391828+Rizqi1580@users.noreply.github.com>
2025-06-12 01:02:14 +0700

    Update and rename singleton.ts to strategy.ts

---
+++ strategy.ts
@@ -0,0 +1,33 @@
+interface StatStrategy {
+  calculate(requests: Request[]): Statistics;
+}
+
+type Request = {
+  time_request: Date;
+  amount: number;
+};
+
+type Statistics = {
+  period: string;
+  totalAmount: number;
+  count: number;
+};
+
+class YearlyStatStrategy implements StatStrategy {
+  calculate(requests: Request[]): Statistics {
+    // Logic hitung statistik per tahun
+    };
+  }
+}
+class MonthlyStatStrategy implements StatStrategy {
+  calculate(requests: Request[]): Statistics {
+    // Logic hitung statistik per bulan
+    };
+  }
+}
+class WeeklyStatStrategy implements StatStrategy {
+  calculate(requests: Request[]): Statistics {
+    // Logic hitung statistik per minggu
+    };
+  }
+}
